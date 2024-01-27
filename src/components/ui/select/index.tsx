c1a8c6e4eedac4476be7be12f53770b8125728af
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-01-27 14:12:53 +0200

    test select

--- src/components/ui/select/index.tsx
+++ src/components/ui/select/index.tsx
@@ -1,14 +1,43 @@
 "use client"
 
-import Select from "react-select"
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Select, { SingleValue } from "react-select"
+
+type TSelectOption = {
+  value: string,
+  label: string
+}
 
 export const CustomSelect = () => {
-  const options = [
+  const [selectedOption, setSelectedOptions] = useState<TSelectOption | null>(null);
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const currentOption = searchParams.get('test')
+  const options: TSelectOption[] = [
     { value: 'chocolate', label: 'Chocolate' },
     { value: 'strawberry', label: 'Strawberry' },
     { value: 'vanilla', label: 'Vanilla' }
   ]
+
+  useEffect(() => {
+    if (currentOption) {
+      const option = options.find(el => el.value === currentOption)
+
+      option && setSelectedOptions(option)
+    } else {
+      setSelectedOptions(null)
+    }
+  }, [])
+
+  const handleSelect = (val: SingleValue<TSelectOption>) => {
+    if (!val) return
+    
+    router.push(`?test=${val.value}`)
+    setSelectedOptions(val)
+  }
   return (
-    <Select options={options}/>
+    <Select options={options}  value={selectedOption}  onChange={handleSelect}/>
+    
   )
 }
