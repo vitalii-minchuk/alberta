12f3b83b6ef7859757e387b6c8bf6bc367be6a56
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-01-27 13:36:22 +0200

    add react-select

---
+++ src/components/ui/select/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Select from "react-select"
+
+export const CustomSelect = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  return (
+    <Select options={options}/>
+  )
+}
