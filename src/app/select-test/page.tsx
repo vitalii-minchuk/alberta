fa0815572d11b6e6688a274fc0bdfc1d3053dee2
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-01-29 08:20:42 +0200

    add suspense to selectors to fix build error

--- src/app/select-test/page.tsx
+++ src/app/select-test/page.tsx
@@ -1,11 +1,14 @@
 import { ProvidersSelect, SortingSelect } from "./components";
 
+import { Suspense } from 'react'
 export default function SelectTestPage() {
     return (
         <div style={{minHeight: '90px'}}>
+ <Suspense>
 
         <ProvidersSelect />
         <SortingSelect />
+ </Suspense>
         </div>
     )
 }
