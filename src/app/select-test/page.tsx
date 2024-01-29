2d7cf52c1f955f346606d00d12bd02a41db1ff95
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-01-29 19:33:51 +0200

    test squash 3

--- src/app/select-test/page.tsx
+++ src/app/select-test/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react'
 export default function SelectTestPage() {
     return (
         <div style={{minHeight: '90px'}}>
-             <Suspense>
+            <Suspense>
                 <ProvidersSelect />
                 <SortingSelect />
             </Suspense>
