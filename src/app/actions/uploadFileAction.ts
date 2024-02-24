f4824ea8e204a4179413e69e48686667c95ce869
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-02-24 11:58:48 +0200

    fix type errors

--- src/app/actions/uploadFileAction.ts
+++ src/app/actions/uploadFileAction.ts
@@ -1,33 +1,43 @@
 "use server"
 
 import { Storage } from '@google-cloud/storage';
-import path from 'path';
 
-const storage = new Storage();
+export default async function uploadFile(formData: FormData) {
+  try {
+   const file = formData.get("audio") as File;
+ 
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function uploadFile(file: File) {
-
-    try {
-        const bucket = ''
-      const destination = `images/${file.name}`;
-
-      // Upload the file to the bucket
-      await storage.bucket(bucket).upload(file, {
-        destination,
-      });
-
-      // Return the public URL of the uploaded file
-      const publicUrl = `${bucket}/${destination}`;
-      return { url: publicUrl }
-    } catch (error) {
-      console.error(error);
-      return { error: 'Failed to upload file' }
-    }
-  
-};
+   if (!file || typeof file === "string") {
+    throw new Error("Audio file not found");
+   }
+   const filePath = file?.name;
+ 
+   const storage = new Storage({
+    projectId: process.env.GCP_PROJECT_ID ?? '',
+    keyFilename: 'google-key.json'
+   });
+   const bucket = storage.bucket(process.env.GCP_BUCKET_NAME ?? '');
+ 
+   const bytes = await file.arrayBuffer();
+   const buffer = Buffer.from(bytes);
+   
+   // Wrap the upload logic in a promise
+   await new Promise((resolve, reject) => {
+    const blob = bucket.file(filePath);
+    const blobStream = blob.createWriteStream({
+     resumable: false,
+    });
+ 
+    blobStream
+     .on("error", (err) => reject(err))
+     .on("finish", () => resolve(true));
+ 
+    blobStream.end(buffer);
+   });
+ 
+   return { success: true }
+  } catch (error) {
+    console.log(error)
+   return { status: 500 }
+  }
+ };
